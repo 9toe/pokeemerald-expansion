e257dc236256e7ba4856ac735d735e966937c9f1
9toe <kylejoeldavidson@gmail.com>
2025-07-13 17:09:12 -0500

    Removed redundant tileset files

--- data/tilesets/exterior_green/decompiled/compile.sh
+++
@@ -1,1 +0,0 @@
-porytiles compile-primary -Wall -tiles-output-pal=true-color -tiles-primary-override=640 -metatiles-primary-override=640 -pals-primary-override=7 -o .. . ../../../../../include/constants/metatile_behaviors.h
